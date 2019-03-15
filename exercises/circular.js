4a71b4963d3db5f7e4b76d77bc5ce83c9604151b
BLOT Martial <martial.blot@outlook.fr>
2019-03-15 12:09:06 +0100

    TDD 4

--- exercises/circular.js
+++ exercises/circular.js
@@ -9,6 +9,9 @@
 
 // Your code :
 
+const circular = new Object()
+circular.circular = circular
+
 //* Begin of tests
 const assert = require('assert')
 
--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -9,11 +9,22 @@
  * Example : "How are you ?" -> "How Are You ?"
  *
  */
+function jadenCase (a){
+    let jad=a.split(" ");
+    for (let i=0; i < jad.length;i++){
+        jad[i]=jad[i].charAt(0).toUpperCase()+jad[i].slice(1);
+    }
+    let jaden = jad.join(" ");
+    return jaden;
+}
 
+//console.log(jadenCase("How are you ?"))
 
 
 //* Begin of tests
 const assert = require('assert')
-
-assert.fail('You must write your own tests')
+assert.strictEqual(typeof jadenCase, 'function')
+assert.strictEqual(jadenCase('How are you ?'),'How Are You ?')
+//assert.strictEqual(jadenCase('Salut toi comment ça va ?'), 'Salut Toi Comment Ça Va ?')
+//assert.fail('You must write your own tests')
 // End of tests */
